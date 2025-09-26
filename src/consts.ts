cbb27ed171bf2ca0e85733ee61385b7364a2ea2f
urpapi <111202315074@mhs.dinus.ac.id>
2025-09-26 15:29:16 +0700

    first init

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'ferdian';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
