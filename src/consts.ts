4448423e96f552bd59638d5e895186e6cd97e827
urpapi <111202315074@mhs.dinus.ac.id>
2025-10-06 21:10:36 +0700

    feat: add data analysis blog post, GridSection component, and update site title in projects view post

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'he.is-a.dev';
+export const SITE_TITLE = 'Ferdian N. Fariza - Data Scientist';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
