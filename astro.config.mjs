b55c4a073ee55d293c444b91939b62af0b66f09d
ferdianfariza <ferdianfariza@gmail.com>
2026-06-14 18:45:12 +0700

    saving

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ferdian.is-a.dev",
+  integrations: [mdx(), sitemap(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/content/ferdian.md
@@ -0,0 +1,7 @@
+---
+title: "Ferdian N. Fariza"
+author: "Ben"
+sex: "Ferdian nur is the best"
+---
+
+Here is my _great_ post!
