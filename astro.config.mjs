d0576937601c623616d1a8293fa995f20916f0b5
ferdianfariza <ferdianfariza@gmail.com>
2026-06-15 12:33:10 +0700

    adding adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // atau "hybrid"
+  adapter: vercel(),
+  output: "server",
   site: "https://ferdian.is-a.dev",
   integrations: [mdx(), sitemap(), react()],
   vite: {
