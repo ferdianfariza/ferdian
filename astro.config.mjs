37ab75275fc1998c909ad6c49c681fbedc1315f9
ferdianfariza <ferdianfariza@gmail.com>
2026-08-07 01:12:05 +0700

    v3 fix final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,24 @@
-// @ts-check
-
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders  } from 'astro/config';
 import sanity from '@sanity/astro';
+import vercel from "@astrojs/vercel";
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  output: "server",
+  site: "https://ferdian.is-a.dev",
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
   integrations: [sanity({
     projectId: "xu9a264e",
     dataset: "production",
     useCdn: false, // for static builds
-  })],
+  }), sitemap()],
 });
